01cf126171381bc64fadad0f7e6695dec8f23eee
rebeselman <rdavila@fi.uba.ar>
2025-08-25 22:20:06 -0300

    agrego constantes

--- mi-generador.py
+++ mi-generador.py
@@ -1,5 +1,35 @@
 import sys
+SERVER = """ server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+        - PYTHONUNBUFFERED=1
+        - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+"""
 
+CLIENT = """ client{ID}:
+    container_name: client{ID}
+    image: client:latest
+    entrypoint: /client
+    environment:
+        - CLI_ID={ID}
+        - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+"""
+
+NETWORK  = """networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"""
 
 # Genera un archivo docker compose con
 # lacantidad de clientes pedidos y el nombre indicado
@@ -8,40 +38,18 @@ def generar_docker_compose(nombre_archivo, cantidad_clientes):
         f.write("name: tp0\n")
         f.write("services:\n")
         f.write("  server:\n")
-        f.write("    container_name: server\n")
-        f.write("    image: server:latest\n")
-        f.write("    entrypoint: python3 /main.py\n")
-        f.write("    environment:\n")
-        f.write("      - PYTHONUNBUFFERED=1\n")
-        f.write("      - LOGGING_LEVEL=DEBUG\n")
-        f.write("    networks:\n")
-        f.write("      - testing_net\n")
+        f.write(SERVER)
+        f.write('\n')
         
-        f.write("\n")
+       
         for i in range(1, cantidad_clientes + 1):
             f.write(f"  client{i}:\n")
-            f.write(f"    container_name: client{i}\n")
-            f.write("    image: client:latest\n")
-            f.write("    entrypoint: /client\n")
-            f.write(f"    environment:\n")
-            f.write(f"      - CLI_ID={i}\n")
-            f.write("      - CLI_LOG_LEVEL=DEBUG\n")
-            f.write("    networks:\n")
-            f.write("      - testing_net\n")
-            f.write("    depends_on:\n")
-            f.write("      - server\n")
-            f.write("\n")
+            f.write(CLIENT.format(ID=i))
+            f.write('\n')
 
 
 
-        f.write("networks:\n")
-        f.write("  testing_net:\n")
-        f.write("    ipam:\n")
-        f.write("      driver: default\n")
-        f.write("      config:\n")
-        f.write("        - subnet: 172.25.125.0/24\n")
-
-
+        f.write(NETWORK)
 
 
 
@@ -51,4 +59,4 @@ if __name__ == "__main__":
     cantidad_clientes = int(sys.argv[2])
 
     generar_docker_compose(nombre_archivo, cantidad_clientes)
-    print(f"Archivo {nombre_archivo} generado con {cantidad_clientes} clientes.")
+   
